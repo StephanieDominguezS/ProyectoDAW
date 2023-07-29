eb539be7859e91e47bb64cc787a0f263ac8c6be6
Ayrton Aldayr <ayrton.24.6.95@gmail.com>
2023-07-29 17:58:10 -0500

    Creacion inicial del proyecto

---
+++ src/main/java/com/proyecto/daw/proyectodaw/ProyectodawApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.daw.proyectodaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectodawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectodawApplication.class, args);
+	}
+
+}
